726cee27195611588efa461eb3b0dfd2076860fd
hyojin425 <gywls5184@gmail.com>
2024-07-08 18:49:28 +0900

    feature: health check 추가

---
+++ src/main/java/umc6th/spring/web/controller/RootController.java
@@ -0,0 +1,13 @@
+package umc6th.spring.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "i'm healthy";
+    }
+}
